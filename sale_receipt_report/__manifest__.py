8ded6c7d42d32b59b7127d216cd288947c613b27
samahkandilict <samahqandeel22@gmail.com>
2024-05-30 18:20:43 +0300

    [ADD]

---
+++ sale_receipt_report/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    "name": "Sale Receipt Report",
+
+    'version': "17.0",
+    'license': 'LGPL-3',
+    'category': 'Sales',
+
+    "summary": "Sales Receipt Report",
+
+    'author': "Samah Knadil",
+    
+    'website': 'https://www.linkedin.com/in/samah-kandil-odoo',
+    'support': 'samahqandeel22@gmail.com',
+
+    "depends": [
+        'base',
+        'sale',
+    ],
+
+    "data": [
+        "views/sale_receipt_report.xml",
+        # "views/report.xml",
+
+    ],
+
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
